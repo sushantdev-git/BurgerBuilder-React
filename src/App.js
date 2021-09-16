7fa935bfd250efd6aab8997869ed079ba7b5ca62
Sushant <sushantkumar6700@gmail.com>
2021-09-16 17:15:50 +0530

    added burger and burgrBuilder to render Burger and also added Burger.css file to render Burger with help of css not with help of image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import  Layout from './components/Layout/Layout'
+import  Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
   render() {
     return (
       <Layout>
+        <BurgerBuilder />
         <p>Hello how are you</p>
       </Layout>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxillary';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <p>Toolbar, Backdrop , navigation</p>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
