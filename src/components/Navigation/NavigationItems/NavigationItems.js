5c731b9c02b5480d5ef51baffedfaccb7e456403
Sushant <sushantkumar6700@gmail.com>
2021-09-17 21:17:45 +0530

    Updated css file for desktop and mobile device

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,12 +5,8 @@ import classes from './NavigationItems.css';
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link = '/' active = {false}>
-            Burger Builder
-        </NavigationItem>
-        <NavigationItem link = '/' active = {false}>
-            Checkout
-        </NavigationItem>
+        <NavigationItem link = '/' active = {false}> Burger Builder </NavigationItem>
+        <NavigationItem link = '/' active = {false}> Checkout</NavigationItem>
     </ul>
 )
 
