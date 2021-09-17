cbc1df2b6a2eddf8125b330d327ca83061e9101b
Sushant <sushantkumar6700@gmail.com>
2021-09-17 20:16:22 +0530

    Added side drawer and Navigatoin Items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavigationItem from './NavigationItem.js/NavigationItem';
+
+import classes from './NavigationItems.css';
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link = '/' active = {false}>
+            Burger Builder
+        </NavigationItem>
+        <NavigationItem link = '/' active = {false}>
+            Checkout
+        </NavigationItem>
+    </ul>
+)
+
+export default navigationItems;
