22163f013a67dd1a97d6543172941e8f6ea20c13
Sushant <sushantkumar6700@gmail.com>
2021-09-18 12:51:41 +0530

    moved layout to higher order component and done some restructuring

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component} from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Auxillary';
+import Aux from '../../../hoc/Auxillary/Auxillary';
 import Backdrop from '../Backdrop/Backdrop'
-const modal = ( props ) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.cancelOrder} />
-        <div className={classes.Modal} 
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0',
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal  extends Component {
+    
+    shouldComponentUpdate(nextProps, nextState){
+        //this modal will only render if we have show property true
+        //show we will check if show property has changed, if yes then React will render this else 
+        //it will not be rendered.
+        return nextProps.show != this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate(){
+        console.log("Modal update")
+    }
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.cancelOrder} />
+                <div className={classes.Modal} 
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0',
+                }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
+export default Modal;
