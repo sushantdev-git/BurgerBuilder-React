4fce6018d5d20b81197ea5f278af9b5be9bd3a19
Sushant <sushantkumar6700@gmail.com>
2021-09-18 17:05:16 +0530

    connected frontend to firebase and storing order data

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL = 'https://burgerbuilder-68afc-default-rtdb.firebaseio.com/'
+    baseURL : 'https://burgerbuilder-68afc-default-rtdb.firebaseio.com/'
 })
 
 export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from '../../axios-orders';
 import Aux from '../../hoc/Auxillary/Auxillary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -50,8 +50,24 @@ class BurgerBuilder extends Component {
     }
 
     continuePurchasing = () => {
-        console.log("buying");
-        alert("Thanks for buying");
+        const order = {
+            user : {
+                name : 'Sushant',
+                email: 'test@test.com',
+                adddress : {
+                    street : 'random',
+                    zipCode: 3943982,
+                    country: 'India',
+                }
+            },
+            delhiveryMethod: 'fastest',
+            ingredients : this.state.ingredients,
+            price : this.state.totalPrice,
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     addItemHandler = (type) => {
