598d5a5390e824959ab8be71b041b45ae6359483
Sushant <sushantkumar6700@gmail.com>
2021-09-20 12:58:49 +0530

    created CheckoutSummary component and installed router for using navigation in app

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css'
+const checkoutSummary = (props) => {
+
+
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope you enjoy this Burger!!</h1>
+            <div >
+                <div style={{width: '100%', margin:"auto"}}>
+                    <Burger ingredients={props.ingredients} haveIngredients={true}/>  
+                </div>
+                <Button btnType="Danger" clicked>CANCEL</Button>
+                <Button btnType="Success" clicked >CONTINUE</Button>
+            </div>
+        </div>
+    )
+}
+
+export default checkoutSummary;
